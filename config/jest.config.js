6192ba9b15333ae18fdbfb9a160f07a20091faf4
Jaya Chakladar <jaya.chakladar@nih.gov>
2025-03-18 10:15:11 -0700

    Add coverage

--- config/jest.config.js
+++ config/jest.config.js
@@ -18,21 +18,22 @@ const config = {
   // clearMocks: false,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
   // coverageDirectory: undefined,
+  coverageDirectory: '../coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
-  // coverageProvider: "babel",
+  coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -178,9 +179,6 @@ const config = {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
-  transform: {
-     '\\.[jt]sx?$': 'babel-jest',
- },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
